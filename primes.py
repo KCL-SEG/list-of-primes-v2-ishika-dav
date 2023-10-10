6159bd5be4798a95d7c353a206c805d7f8d26734
Ishika Dav <ishikadav03@gmail.com>
2023-10-10 16:27:12 +0100

    Changed so no user input required for program to run

--- primes.py
+++ primes.py
@@ -7,29 +7,31 @@ def is_prime(num):
         return True
     if num % 2 == 0:
         return False
-    for i in range(3, int(num**0.5) + 1, 2):
+    for i in range(3, num):
         if num % i == 0:
             return False
     return True
 
 def primes(number_of_primes):
-    if number_of_primes < 1:
-        raise ValueError("Number of primes must be a positive integer.")
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer greater than one")
     
-    list = []
+    prime_list = []
     num = 2  
-    while len(list) < number_of_primes:
+    while len(prime_list) < number_of_primes:
         if is_prime(num):
-            list.append(num)
+            prime_list.append(num)
         num += 1
-    return list
+    return prime_list
 
 try:
-    number_of_primes = int(input("How many prime numbers do you want? "))
+    number_of_primes = 20
     prime_numbers = primes(number_of_primes)
     print(prime_numbers)  
+
 except ValueError as e:
-    print("Error: ",e)    
+    print("Error:",e)  
+  
 
 
 
